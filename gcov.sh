c8fc0c56f3444c8354191fc5b4952bf2fe97f378
Darby Johnston <darbyjohnston@yahoo.com>
2020-04-22 14:21:08 -0700

    More gcov work

--- gcov.sh
+++ gcov.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 gcnoList=`find $1 -name "*.gcno" -and \( -not -name "CMakeC*CompilerId.gcno" \)`
-echo "gcnoList="$gcnoList
+#echo "gcnoList="$gcnoList
 for gcno in $gcnoList
 do
     srcName=`basename -s .gcno $gcno`
-    echo "srcName="$srcName
+    #echo "srcName="$srcName
     src=`find $1 -name $srcName`
-    echo "src="$src
+    #echo "src="$src
     gcov -o $gcno $src
 done
